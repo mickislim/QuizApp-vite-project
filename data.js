05dfc9fb1ae7fac2007bfeaa1a839c95209b4e95
mickislim <127298757+mickislim@users.noreply.github.com>
2023-12-14 08:54:43 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+export const data =[{
+    
+        question:"Who was the first woman to be a pilot?",
+        answer:[
+            {text:'Mrs Fela Kuti', correct:false,},
+            {text:"Mrs Chinyere Onyemucheya", correct:true },
+    { text:'Mrs Aisha Buhari', correct:false},
+    { text:"Mrs Kemi Adeosun", correct:false }]
+
+
+
+    
+},
+{
+    
+    question:"Who was the first woman to drive a car?",
+    answer:[
+        {text:'Mrs Olufunmi Ransome Kuti', correct:true},
+        {text:"Mrs Chinyere Onyemucheya", correct:false },
+{ text:'Mrs Aisha Buhari', correct:false},
+{ text:"Mrs Kemi Adeosun", correct:false }]
+
+
+
+
+},
+
+{
+    
+    question:"Who formed the first political party in Nigeria?",
+    answer:[
+        {text:'President Olusegun Obasanjo', correct:false},
+        {text:"Goodluck Jonathan", correct:false },
+{ text:'Mrs Aisha Buhari', correct:false},
+{ text:"Herbert Macauly", correct:true }]
+
+
+
+    }
+
+
+]
